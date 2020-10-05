03c6de26684e9a81552100a27ddabc3cefe03515
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-10-05 22:52:16 +0530

    add: no wallpaper and retry func

--- app/src/main/java/com/mrwhoknows/wallstack/screens/home/WallGalleryFragment.kt
+++ app/src/main/java/com/mrwhoknows/wallstack/screens/home/WallGalleryFragment.kt
@@ -2,9 +2,11 @@ package com.mrwhoknows.wallstack.screens.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
 import com.mrwhoknows.wallstack.R
 import com.mrwhoknows.wallstack.adapter.WallpaperListAdapter
@@ -23,7 +25,7 @@ class WallGalleryFragment : Fragment(R.layout.fragment_wallpaper_list) {
         val adapter = WallpaperListAdapter()
 
         recyclerView.setHasFixedSize(true)
-        val layoutManager = GridLayoutManager(requireContext(),2)
+        val layoutManager = GridLayoutManager(requireContext(), 2)
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
             header = WallpaperLoadStateAdapter { adapter.retry() },
@@ -33,5 +35,23 @@ class WallGalleryFragment : Fragment(R.layout.fragment_wallpaper_list) {
         viewModel.wallpapers.observe(viewLifecycleOwner, Observer { wallpapersPage ->
             adapter.submitData(viewLifecycleOwner.lifecycle, wallpapersPage)
         })
+
+        adapter.addLoadStateListener { loadState ->
+            progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+            recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+            retryButton.isVisible = loadState.source.refresh is LoadState.Error
+            errorEmptyTv.isVisible = loadState.source.refresh is LoadState.Error
+
+            //    empty view
+            if (loadState.source.refresh is LoadState.NotLoading &&
+                loadState.append.endOfPaginationReached &&
+                adapter.itemCount < 1
+            ) {
+                recyclerView.isVisible = false
+                errorEmptyTv.isVisible = true
+            } else {
+                errorEmptyTv.isVisible = false
+            }
+        }
     }
 }
