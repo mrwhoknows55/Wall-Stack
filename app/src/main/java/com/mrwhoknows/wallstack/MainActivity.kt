80bda340a0efbc4c43d8f90a69bc61492b9df954
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2020-12-19 22:07:57 +0530

    add: set, download buttons in bottom sheet

--- app/src/main/java/com/mrwhoknows/wallstack/MainActivity.kt
+++ app/src/main/java/com/mrwhoknows/wallstack/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mrwhoknows.wallstack.screens.home.WallGalleryViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,11 +17,16 @@ class MainActivity : AppCompatActivity() {
 
     val wallGalleryViewModel by viewModels<WallGalleryViewModel>()
     lateinit var topToolbar: MaterialToolbar
+    lateinit var topAppbarLayout: AppBarLayout
+    lateinit var bottomNavBar: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         topToolbar = findViewById(R.id.topAppBar)
+        bottomNavBar = findViewById(R.id.bottomNavBar)
+        topAppbarLayout = findViewById(R.id.topAppBarLayout)
 
         NavigationUI.setupWithNavController(bottomNavBar, navHostFragment.findNavController())
 
--- app/src/main/java/com/mrwhoknows/wallstack/screens/wallpaper/WallpaperFragment.kt
+++ app/src/main/java/com/mrwhoknows/wallstack/screens/wallpaper/WallpaperFragment.kt
@@ -1,34 +1,75 @@
 package com.mrwhoknows.wallstack.screens.wallpaper
 
-import android.app.WallpaperManager
-import android.graphics.drawable.BitmapDrawable
+import android.app.DownloadManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.os.Environment
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import coil.load
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.mrwhoknows.wallstack.MainActivity
 import com.mrwhoknows.wallstack.R
 import com.mrwhoknows.wallstack.db.FavWall
 import com.mrwhoknows.wallstack.db.FavWallDatabase
 import kotlinx.android.synthetic.main.fragment_wallpaper.*
+import kotlinx.android.synthetic.main.set_wallpaper_sheet.*
 import kotlinx.coroutines.launch
+import java.io.File
 
 class WallpaperFragment : Fragment(R.layout.fragment_wallpaper) {
 
     private lateinit var args: WallpaperFragmentArgs
+    private lateinit var topToolbar: MaterialToolbar
+    private lateinit var bottomNavBar: BottomNavigationView
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
+    private lateinit var wallUri: Uri
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         args = WallpaperFragmentArgs.fromBundle(requireArguments())
 //        TODO: make it transparent
-//        val topToolbar = (requireActivity() as MainActivity).topToolbar
-//        topToolbar.visibility = View.GONE
-//        setWallFAB.hide()
-        setWallFAB.setOnClickListener {
-            setWallpaper()
+        val mainActivity = requireActivity() as MainActivity
+        topToolbar = mainActivity.topToolbar
+        bottomNavBar = mainActivity.bottomNavBar
+        mainActivity.topAppbarLayout.setExpanded(false)
+        topToolbar.visibility = View.GONE
+        bottomNavBar.visibility = View.GONE
+
+        val bottomSheet = setWallSheet
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+        setWallBtn.setOnClickListener {
+
+            setWallpaper(wallUri)
+        }
+
+//        TODO Check Permission
+        downloadWallpaper.setOnClickListener {
+            val fileName = "wallstack-${args.wallpaperId}.jpg"
+            val downloadManager: DownloadManager =
+                requireActivity().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            val uri = Uri.parse(args.wallpaperURL)
+            val request: DownloadManager.Request = DownloadManager.Request(uri)
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_ONLY_COMPLETION)
+            request.setDestinationInExternalPublicDir(
+                Environment.DIRECTORY_PICTURES,
+                "WallStack/$fileName"
+            )
+            downloadManager.enqueue(request)
+            val file = File(
+                "${Environment.getExternalStorageDirectory().path}/${Environment.DIRECTORY_PICTURES}/WallStack",
+                fileName
+            )
+            wallUri = Uri.parse(file.absolutePath)
         }
 
         wallpaperImageView.load(args.wallpaperURL) {
@@ -38,7 +79,7 @@ class WallpaperFragment : Fragment(R.layout.fragment_wallpaper) {
         }
 
 
-//        TODO: do this when clicked on like btn
+//        TODO: 1 do this when clicked on like btn
         val dao = FavWallDatabase.getInstance(requireContext()).favWallDao()
         lifecycleScope.launch {
 //            TODO change to id
@@ -48,17 +89,35 @@ class WallpaperFragment : Fragment(R.layout.fragment_wallpaper) {
 
     }
 
-    private fun setWallpaper() {
+//      TODO: 2. get back to old method with  home, lock screen and both as option dialog
+    private fun setWallpaper(wallUri: Uri) {
         try {
-            wallpaperImageView.invalidate()
-            val drawable = wallpaperImageView.drawable as BitmapDrawable
-            val wallBitmap = drawable.bitmap
-            val manager = WallpaperManager.getInstance(activity)
-            manager.setBitmap(wallBitmap)
-            Toast.makeText(context, "Wallpaper Set!", Toast.LENGTH_SHORT).show()
+//            TODO old code for ref
+//            wallpaperImageView.invalidate()
+//            val drawable = wallpaperImageView.drawable as BitmapDrawable
+//            val wallBitmap = drawable.bitmap
+//            val manager = WallpaperManager.getInstance(activity)
+//            manager.setBitmap(wallBitmap)
+
+            val intent = Intent(Intent.ACTION_ATTACH_DATA).apply {
+                setDataAndType(wallUri, "image/jpeg")
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                putExtra("mimeType", "image/*")
+            }
+            startActivity(
+                Intent.createChooser(intent, "Set as: ").addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            )
+
         } catch (e: Exception) {
             Toast.makeText(context, "$e.localizedMessage}", Toast.LENGTH_SHORT).show()
             e.printStackTrace()
         }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        topToolbar.visibility = View.VISIBLE
+        bottomNavBar.visibility = View.VISIBLE
     }
 }
